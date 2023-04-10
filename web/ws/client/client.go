e62d0efe1e992b9b72b7a277c2e3fdf890dc28f2
Andy <andy9815040002@gmail.com>
2023-04-10 15:51:11 +0800

    [#2]Init game server config

--- web/ws/client/client.go
+++ web/ws/client/client.go
@@ -2,10 +2,10 @@ package client
 
 import (
 	"errors"
+	"game_server/common/web/response"
 	"log"
 	"sync/atomic"
 	"time"
-	"xxx/common/web/response"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
