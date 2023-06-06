92003de783c0a29a482dab6056700791897b4aeb
Andy <andy9815040002@gmail.com>
2023-06-06 12:11:41 +0800

    [#10]Add batch redis lock and unlock.

--- redis/redis_test.go
+++ redis/redis_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"testing"
+	"time"
 	"xxx/common/configs"
 	"xxx/common/logs"
 )
@@ -199,4 +200,30 @@ func TestRedisLock(t *testing.T) {
 	if isLock := RedisUnlock(WOW_GAMING_MUTEX_PREFIX, key); !isLock {
 		t.Fatal("Redis lock  unlock failed")
 	}
+}
+
+func TestExist(t *testing.T) {
+	InitRedis(context.Background())
+
+	key := "key_test_exist"
+
+	if IsExist(key) {
+		info, ok := GetString(key)
+		if !ok {
+			t.Fatal("get redis failed")
+		} else {
+			t.Fatal("info:", info)
+		}
+		t.Fatal("Key can't exist")
+	}
+
+	if err := Put(key, 1, time.Second); err != nil {
+		t.Fatal("Put key failed. err:", err.Error())
+	}
+
+	if !IsExist(key) {
+		t.Fatal("Key can't find")
+	}
+
+	t.Log("Exist finish")
 }
