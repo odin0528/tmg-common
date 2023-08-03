5f231946ded0b19df573ee9c6be08e66885d7f1c
Alan Lu <alan0098@gmail.com>
2023-08-03 17:55:37 +0800

    1. Remove MongoDB related functions. 2. Add a new function to format time.

--- utils/definition.go
+++ utils/definition.go
@@ -24,7 +24,6 @@ const (
 	TIME_FORMAT                                = "2006-01-02 15:04:05"
 	TIME_FORMAT_WITH_MICRO_SEC          string = "2006-01-02 15:04:05.999999"
 	TIME_FORMAT_WITH_MICRO_SEC_TIMEZONE string = "2006-01-02 15:04:05.999999 -0700"
-	MONGODB_TIME_FORMAT                 string = "2006-01-02 15:04:05.999999999 -0700 MST"
 	QUERY_TIME_FLOAT_LIMIT                     = 999999000
 )
 
