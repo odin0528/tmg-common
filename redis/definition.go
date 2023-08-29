20f4feed55fd6a4ce5c978df775a7fa026ce76cd
Alan Lu <alan0098@gmail.com>
2023-08-29 16:55:00 +0800

    Fixing typo.

--- redis/definition.go
+++ redis/definition.go
@@ -40,7 +40,7 @@ const (
 
 	AGENT_ID_SERIAL_NUMBER_PREFIX string = "agent_id_serial_num_"
 
-	LOGIN_DETAIL_HASH_KEY string = "cms_login_has_key"
+	LOGIN_DETAIL_HASH_KEY string = "cms_login_hash_key"
 
 	LOGIN_ACCOUNT_HASH_KEY string = "cms_login_account_has_key"
 
