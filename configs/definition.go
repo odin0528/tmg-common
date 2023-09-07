0b2ea7acde9433deb9bfd200c72c769efb385806
Alan Lu <alan0098@gmail.com>
2023-09-07 15:45:09 +0800

    Add allow headers.

--- configs/definition.go
+++ configs/definition.go
@@ -28,6 +28,7 @@ const (
 
 	WEB_API_API_CENTER_DOMAIN             string = "api_center_domain"
 	WEB_API_ALLOW_CORS_DOMAIN             string = "allowed_cors_domain"
+	WEB_API_ALLOW_CORS_HEADERS            string = "allowed_cors_headers"
 	WEB_API_JWT_SECRET_KEY_DURATION_HOURS string = "jwt_secret_key_duration_hours"
 	WEB_API_AUTH_TOKEN_DURATION_HOURS     string = "auth_token_duration_hours"
 
