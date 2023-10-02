21e8ba386715a889d344f7ef1da7ba66d05f8614
Wayde <iamway902@yahoo.om.tw>
2023-10-02 12:17:42 +0800

    add seven dragons code and msg

--- web/response/message_definition.go
+++ web/response/message_definition.go
@@ -46,6 +46,7 @@ const (
 	MSG_ROOM_IS_FINISH   = "room is finish"
 	MSG_GAME_IS_NOT_INIT = "game is not init"
 	MSG_NOT_IN_STATE     = "not in %s state, current state: %s"
+	MSG_IS_NOT_FREE_GAME = "game is not free game"
 )
 
 // internal
