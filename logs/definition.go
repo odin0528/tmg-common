31bf13c4c77fe6dfce7f704ac4568e003e3debbe
Andy <andy9815040002@gmail.com>
2024-05-08 16:06:11 +0800

    add log key

--- logs/definition.go
+++ logs/definition.go
@@ -60,6 +60,7 @@ const (
 	LOG_KEY_API                 = "api"
 	LOG_KEY_SINGLE_WALLET       = "single_wallet"
 	LOG_KEY_ROLE                = "role"
+	LOG_KEY_RISK_CONTROL        = "risk_control"
 )
 
 const (
