b88bfa0817d6a8b75f2f04f5f350f3bac6ee5946
Ohmimi <gash60535@gmail.com>
2024-07-11 02:12:26 +0800

    update pickupWeight v2

--- math_tool/random.go
+++ math_tool/random.go
@@ -3,6 +3,7 @@ package math_tool
 import (
 	"crypto/rand"
 	"math/big"
+	math_rand "math/rand"
 )
 
 func GetRandomFloat64(max float64) float64 {
@@ -34,4 +35,29 @@ func GetShuffleCopyArray[T any](target []T) []T {
 	copy(n, target)
 	Shuffle(n)
 	return n
+}
+
+// 取區間: min ~ max-1 隨機數(不包含max)
+func GetRandomRangeNum(r *math_rand.Rand, min, max int) int {
+	var result int
+	switch {
+	case min > max:
+		min, max = max, min
+		maxRand := max - min
+		b := r.Intn(maxRand)
+		result = min + b
+	case max == min:
+		result = max
+	case max > min:
+		maxRand := max - min
+		b := r.Intn(maxRand)
+		result = min + b
+	}
+
+	return result
+}
+
+// 取區間: min ~ max 隨機數(包含max)
+func GetRandomRangeNumWithMax(r *math_rand.Rand, min, max int) int {
+	return GetRandomRangeNum(r, min, max+1)
 }
