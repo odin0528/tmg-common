57cea1c62120b9f6b816b32955196ab1bf8dfb50
AdamZhang <adamzhang228@gmail.com>
2024-09-19 02:05:15 +0800

    Introduce KNOWN_COUNTRY

--- geoip/geoip.go
+++ geoip/geoip.go
@@ -2,8 +2,8 @@ package geoip
 
 import (
 	"errors"
-	"xxx/common/configs"
 	"net"
+	"xxx/common/configs"
 
 	"github.com/oschwald/geoip2-golang"
 )
@@ -23,12 +23,12 @@ func InitGeoIPRepository() error {
 
 func GetCountry(ip net.IP) (string, error) {
 	if reader == nil {
-		return "", errors.New("reader is not initialized")
+		return UNKNOWN_COUNTRY, errors.New("reader is not initialized")
 	}
 
 	record, err := reader.Country(ip)
 	if err != nil {
-		return "", err
+		return UNKNOWN_COUNTRY, err
 	}
 
 	return record.Country.IsoCode, nil
