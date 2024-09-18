4607f3c2faec15bd78f656bec37606bc6be8edca
domonkasshu <domon80501@gmail.com>
2024-09-18 14:58:08 +0800

    1.Change context format in annonucement struct.

--- redis/definition.go
+++ redis/definition.go
@@ -127,17 +127,17 @@ type GameCountingBuyFeatureInfo struct {
 }
 
 type Announcement struct {
-	Id               int        `json:"id"`
-	Title            string     `json:"title"`
-	Context          string     `json:"context"`
-	Mode             int        `json:"mode"`
-	TargetRule       string     `json:"target_rule"`
-	AnnounceStartAt  *time.Time `json:"announce_start_at"`
-	AnnounceEndAt    *time.Time `json:"announce_end_at"`
-	RedirectGameName string     `json:"redirect_game_name"`
-	Duration         int        `json:"duration"`
-	NextAnnounceAt   time.Time  `json:"next_announce_at"`
-	NotifyCount      int        `json:"notify_count"`
+	Id               int               `json:"id"`
+	Title            string            `json:"title"`
+	Context          map[string]string `json:"context"`
+	Mode             int               `json:"mode"`
+	TargetRule       string            `json:"target_rule"`
+	AnnounceStartAt  *time.Time        `json:"announce_start_at"`
+	AnnounceEndAt    *time.Time        `json:"announce_end_at"`
+	RedirectGameName string            `json:"redirect_game_name"`
+	Duration         int               `json:"duration"`
+	NextAnnounceAt   time.Time         `json:"next_announce_at"`
+	NotifyCount      int               `json:"notify_count"`
 }
 
 type DeviceCountingFromLogin struct {
