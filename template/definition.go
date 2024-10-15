bea3e195e27b6805e7f4584b29afa1c01f44ead1
domonkasshu <domon80501@gmail.com>
2024-10-15 15:39:53 +0800

    1.Modified announcement struct. 2.Add const values.

--- template/definition.go
+++ template/definition.go
@@ -9,3 +9,12 @@ const (
 	CAMP_WAR_AHEAD_OF_OPPONENT                = 6
 	CAMP_WAR_IS_ABOUT_TO_WIN                  = 7
 )
+
+const (
+	REPLACE_INFO_TIME_RANGE_TEXT = "time_range_text"
+	REPLACE_INFO_GAME_NAME       = "game_name"
+	REPLACE_INFO_PLAYER_ACCOUNT  = "player_account"
+	REPLACE_INFO_JACKPOT_NAME    = "jackpot_name"
+	REPLACE_INFO_AWARD           = "award"
+	REPLACE_INFO_MULTIPLIER      = "multiplier"
+)
