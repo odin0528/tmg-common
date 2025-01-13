61008f40d175be794a45c9a74d6c520246fa3f93
Andy <andy9815040002@gmail.com>
2025-01-13 20:15:40 +0800

    Add redis define.

--- redis/definition.go
+++ redis/definition.go
@@ -112,6 +112,9 @@ const (
 	LOGIN_TYPE_COUNT_HASH_KEY = "player_login_type_count_hash_key"
 	LOGIN_TYPE_LOBBY          = "login_type_lobby"
 	LOGIN_TYPE_GAME           = "login_type_game"
+
+	AGENT_USE_API_HASH_KEY  = "agent_use_api_hash_key"
+	SERVER_ADD_NEW_GAME_KEY = "server_add_new_game_key"
 )
 
 const (
