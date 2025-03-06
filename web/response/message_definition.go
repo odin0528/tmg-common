1f05e9087b0665f6b3b6c4c63fdaa61b6ecaf6ff
domonkasshu <domon80501@gmail.com>
2025-03-06 19:00:48 +0800

    1.Add key.

--- web/response/message_definition.go
+++ web/response/message_definition.go
@@ -72,6 +72,7 @@ const (
 	MSG_DUPLICATE_TOKEN          = "player is duplicate"
 	MSG_BET_AREA_IS_LOCK         = "bet area is lock"
 	MSG_ROOM_IS_NOT_FOUND        = "room is not found"
+	MSG_GAME_CONTROL_TYPE_ERROR  = "game occur error"
 )
 
 // internal
