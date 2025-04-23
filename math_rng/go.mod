37171873eb66e790c2bbf82f7dd31fa9a4644bb1
adamzhang <adamzhang228@gmail.com>
2025-04-23 18:01:26 +0800

    Revert "Create math_rng as go module"
    This reverts commit ad27c4468f2e9dc2f5512ed26e1a8a14e76e5ad7.

--- math_rng/go.mod
+++
@@ -1,3 +0,0 @@
-module gltw.6633663.com/Backend_GSGame/common/math_rng
-
-go 1.19
--- math_tool/rng.go
+++ math_tool/rng.go
@@ -1,21 +1,60 @@
 package math_tool
 
 import (
-	"gltw.6633663.com/Backend_GSGame/common/math_rng"
+	"crypto/rand"
+	"log"
+	"math"
+	"math/big"
 )
 
 func GetRandomInt64(max int64) int64 {
-	return math_rng.GetRandomInt64(max)
+	if max <= 0 {
+		return 0
+	}
+	bigInt := new(big.Int).SetInt64(int64(max))
+	i, _ := rand.Int(rand.Reader, bigInt)
+	return i.Int64()
 }
 
 func GetRandomInt(max int) int {
-	return math_rng.GetRandomInt(max)
+	return int(GetRandomInt64(int64(max)))
 }
 
 func Shuffle[T any](target []T) {
-	math_rng.Shuffle(target)
+	for i := range target {
+		j := GetRandomInt64(int64(i) + 1)
+		target[i], target[j] = target[j], target[i]
+	}
 }
 
 func PickByWeights(weights []float64) (pickIdx int) {
-	return math_rng.PickByWeights(weights)
+	if len(weights) == 0 {
+		log.Println("weights is empty")
+		return 0
+	}
+
+	sum := 0.0
+	maxLimitList := make([]float64, len(weights))
+
+	for idx, weight := range weights {
+		sum += weight
+		maxLimitList[idx] = sum
+	}
+
+	sum *= math.Pow10(6)
+
+	bigInt := new(big.Int).SetInt64(int64(sum))
+	v, _ := rand.Int(rand.Reader, bigInt)
+
+	value := float64(v.Int64()) * math.Pow10(-6)
+
+	pickIdx = 0
+	for idx, limit := range maxLimitList {
+		if IsFloatLessThan(value, limit) {
+			pickIdx = idx
+			break
+		}
+	}
+
+	return pickIdx
 }
