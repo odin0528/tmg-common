c45f11168cd8c6a0f0c3235425dd8318b7406491
adamzhang <adamzhang228@gmail.com>
2025-05-19 15:22:34 +0800

    Refine PipelineWrapper & TxPipelineWrapper

--- redis/redis_pipeline.go
+++ redis/redis_pipeline.go
@@ -1,15 +1,29 @@
 package redis
 
 import (
+	"context"
 	"time"
 )
 
-func (rp *redisPipelineWrapper) Set(key string, value interface{}, expiration time.Duration) {
-	cmd := rp.pipe.Set(rp.ctx, key, value, expiration)
+func (rp *PipelineWrapper) Set(key string, value interface{}, expiration time.Duration) {
+	cmd := rp.pipe.Set(context.TODO(), key, value, expiration)
 	rp.cmds = append(rp.cmds, cmd)
 }
 
-func (rp *redisPipelineWrapper) Get(key string) {
-	cmd := rp.pipe.Get(rp.ctx, key)
+func (rp *PipelineWrapper) Get(key string) {
+	cmd := rp.pipe.Get(context.TODO(), key)
 	rp.cmds = append(rp.cmds, cmd)
+}
+
+func (rp *PipelineWrapper) Exec(ctx context.Context) ([]CmdResult, error) {
+	cmds, err := rp.pipe.Exec(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return processPipelineResults(cmds), nil
+}
+
+func (rp *PipelineWrapper) Discard() error {
+	return nil
 }
