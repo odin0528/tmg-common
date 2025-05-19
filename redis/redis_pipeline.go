a256dc02f8ea69b6f5d7c3b3c0c7b39488c930df
Rosco <linchi@linqideMacBook-Air.local>
2025-05-19 16:22:36 +0800

    pipline add HSET.HGET

--- redis/redis_pipeline.go
+++ redis/redis_pipeline.go
@@ -27,3 +27,13 @@ func (rp *PipelineWrapper) Exec(ctx context.Context) ([]CmdResult, error) {
 func (rp *PipelineWrapper) Discard() error {
 	return nil
 }
+
+func (rp *PipelineWrapper) HSet(key string, field string, value interface{}) {
+	cmd := rp.pipe.HSet(context.TODO(), key, field, value)
+	rp.cmds = append(rp.cmds, cmd)
+}
+
+func (rp *PipelineWrapper) HGet(key string, field string) {
+	cmd := rp.pipe.HGet(context.TODO(), key, field)
+	rp.cmds = append(rp.cmds, cmd)
+}
