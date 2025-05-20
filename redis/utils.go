cb3729b6e9faab3cafb68a174da35c67ac546764
riley.wu <riley.wu@bsump.com>
2025-05-20 17:43:37 +0800

    remove getscriptkey

--- redis/utils.go
+++ redis/utils.go
@@ -2,10 +2,8 @@ package redis
 
 import (
 	"fmt"
-	"strings"
 	"time"
 	"xxx/common/utils"
-	"xxx/pkg/model"
 )
 
 func GetPlayerBetInfoHashKey(account string) string {
@@ -52,39 +50,6 @@ func GetDailyBetRankKey(gameName, currency string) string {
 
 	cacheKey := GetCacheKey(DAILY_BET_RANK_KEY, date, gameName, currency)
 	return cacheKey
-}
-
-func GetScriptKey(gameName, tableName string, oddsType int) string {
-	return fmt.Sprintf("%s:%s:%s:%d",
-		RISK_CONTROL_SCRIPT_KEY,
-		strings.ToLower(gameName),
-		tableName,
-		oddsType,
-	)
-}
-
-func GetScriptKeyWithCondition(gameName, tableName, condition string, oddsType int) string {
-	return fmt.Sprintf("%s:%s:%s:%s:%d",
-		RISK_CONTROL_SCRIPT_KEY,
-		strings.ToLower(gameName),
-		tableName,
-		condition,
-		oddsType,
-	)
-}
-
-func GetScriptKeyByGame(gameName, tableName, condition string, featureIndex, oddsType int) string {
-	switch strings.ToUpper(gameName) {
-	//bonus也有condition的話跑第一個
-	case model.GAME_NAME_BIG_FIVE_GAME:
-		return GetScriptKeyWithCondition(gameName, tableName, condition, oddsType)
-	default:
-		if featureIndex == 0 {
-			return GetScriptKeyWithCondition(gameName, tableName, condition, oddsType)
-		} else {
-			return GetScriptKey(gameName, tableName, oddsType)
-		}
-	}
 }
 
 func GetScriptKeyWithModeAndFeatureType(gameName string, featureIndex, featureSecondIndex, mode, featureType int) string {
