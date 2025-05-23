a10c641317b68f294b780c59c046d335ee20cf52
xxxVitoxxx <bird06291994@gmail.com>
2025-05-23 16:59:26 +0800

    add mail title and context config of barong

--- template/definition.go
+++ template/definition.go
@@ -1,16 +1,18 @@
 package template
 
 const ( //模板編號 //用字串 未來分類或做其他變化比較方便
-	ANNOUNCEMENT_SYSTEM_EMERGENCY_MAINTENANCE            = "1"
-	ANNOUNCEMENT_NORMAL_MAINTENANCE                      = "2"
-	ANNOUNCEMENT_REWARD_JP                               = "3"
-	ANNOUNCEMENT_REWARD_X                                = "4"
-	CAMP_WAR_NEED_SUPPORT                                = "5"
-	CAMP_WAR_AHEAD_OF_OPPONENT                           = "6"
-	CAMP_WAR_IS_ABOUT_TO_WIN                             = "7"
-	MAIL_TITLE_CAMP_WAR_TITAN_AWARD                      = "8"
-	MAIL_CONTEXT_CAMP_WAR_TITAN_AWARD                    = "9"
-	MAIL_CONTEXT_CAMP_WAR_TITAN_AWARD_FROM_SYSTEM_ASSIGN = "10"
+	ANNOUNCEMENT_SYSTEM_EMERGENCY_MAINTENANCE             = "1"
+	ANNOUNCEMENT_NORMAL_MAINTENANCE                       = "2"
+	ANNOUNCEMENT_REWARD_JP                                = "3"
+	ANNOUNCEMENT_REWARD_X                                 = "4"
+	CAMP_WAR_NEED_SUPPORT                                 = "5"
+	CAMP_WAR_AHEAD_OF_OPPONENT                            = "6"
+	CAMP_WAR_IS_ABOUT_TO_WIN                              = "7"
+	MAIL_TITLE_CAMP_WAR_TITAN_AWARD                       = "8"
+	MAIL_CONTEXT_CAMP_WAR_TITAN_AWARD                     = "9"
+	MAIL_CONTEXT_CAMP_WAR_TITAN_AWARD_FROM_SYSTEM_ASSIGN  = "10"
+	MAIL_TITLE_CAMP_WAR_BARONG_AWARD                      = "11"
+	MAIL_CONTEXT_CAMP_WAR_BARONG_AWARD_FROM_SYSTEM_ASSIGN = "12"
 )
 
 const ( //取代key-name
