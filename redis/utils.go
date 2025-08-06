4fd5303be34b5e09f4738777aa0a20eb0a4d79b8
domonkasshu <domon80501@gmail.com>
2025-08-06 16:40:59 +0800

    Revert "1.optimize GetCacheKey"
    This reverts commit 9af405a89ca5680ac1241c70d797364a81769fb1.

--- redis/utils.go
+++ redis/utils.go
@@ -3,7 +3,6 @@ package redis
 import (
 	"fmt"
 	"strconv"
-	"strings"
 	"time"
 	"xxx/common/utils"
 )
@@ -13,14 +12,16 @@ func GetPlayerBetInfoHashKey(account string) string {
 }
 
 func GetCacheKey(keys ...string) string {
-	var builder strings.Builder
+	result := ""
 	for i, key := range keys {
-		builder.WriteString(key)
-		if i != len(keys)-1 {
-			builder.WriteString(":")
+		result += key
+
+		if i != (len(keys) - 1) {
+			result += ":"
 		}
 	}
-	return builder.String()
+
+	return result
 }
 
 func GetAllCacheKey(keys ...string) string {
