fa02e5495397baa4ba6220561ab8b09610f35ade
Andy <andy9815040002@gmail.com>
2025-09-11 18:16:51 +0800

    Add enable cors define

--- configs/definition.go
+++ configs/definition.go
@@ -65,6 +65,7 @@ const (
 	WEB_API_ALLOW_CORS_DOMAIN                string = "allowed_cors_domain"
 	WEB_API_SLOT_MACHINE_DOMAIN              string = "slot_machine_domain"
 	WEB_API_ALLOW_CORS_HEADERS               string = "allowed_cors_headers"
+	WEB_API_CORS_ENABLE                      string = "cors_enable"
 	WEB_API_JWT_SECRET_KEY_DURATION_HOURS    string = "jwt_secret_key_duration_hours"
 	WEB_API_AUTH_TOKEN_DURATION_HOURS        string = "auth_token_duration_hours"
 	WEB_API_ENABLE_TRUST_IP                  string = "enable_trust_ip"
