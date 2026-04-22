d69ad492a521889ca41330767fbaac9a78aa4ad9
domonkasshu <domon80501@gmail.com>
2026-04-22 12:16:43 +0800

    1.add redis key.

--- redis/definition.go
+++ redis/definition.go
@@ -164,6 +164,8 @@ const (
 	REDIS_KEY_REVERSE_TRANSACTION_PLAYER_TTL = time.Hour * 24 * 7
 
 	RISK_POOL_CLEAN_RANDOM_DATE_KEY = "risk_pool_clean_random_date"
+
+	AGENT_GAME_RTP_SETTING_HASH_MAP_KEY = "agent_game_rtp_settings"
 )
 
 const (
