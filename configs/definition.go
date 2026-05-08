7a353521788e423226b97f7b021ca14191d9fa67
JC <ncues0222020@gmail.com>
2026-05-08 10:01:39 +0800

    add service_namespace

--- configs/definition.go
+++ configs/definition.go
@@ -60,6 +60,7 @@ const (
 	SYSTEM_ENV                             string = "env"
 	SYSTEM_BET_RECORD_SPLIT_START_DATETIME string = "bet_record_split_start_datetime"
 	SYSTEM_NAMESPACE                       string = "namespace"
+	SYSTEM_SERVICE_NAMESPACE               string = "service_namespace"
 
 	WEB_API_API_CENTER_DOMAIN                  string = "api_center_domain"
 	WEB_API_GAME_SERVER_DOMAIN                 string = "game_server_domain"
